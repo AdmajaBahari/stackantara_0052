5c31d9ff7172fd0ca6ea70f58602fd72df3e3e0d
AdmajaBahari <161295956+AdmajaBahari@users.noreply.github.com>
2026-08-13 10:17:26 +0700

    9st commit

--- stack.cpp
+++ stack.cpp
@@ -45,4 +45,22 @@ public:
         cout << "Popped Value: " << top->data << endl;
         top = top->next;
     }
+
+    //9. pembuatan prosedur peek
+    void peek() {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node* current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
 };
