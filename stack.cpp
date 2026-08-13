7c7367e225ff64aff5a6a5f8ab6d74b6552512d8
AdmajaBahari <161295956+AdmajaBahari@users.noreply.github.com>
2026-08-13 10:13:50 +0700

    2st commit

--- stack.cpp
+++ stack.cpp
@@ -4,4 +4,7 @@ using namespace std;
 //1. inisialisasi class node
 class Node {
 public:
+    //2. inisialisasi variabel data dan Node next sebagai member class Node
+    int data;
+    Node* next;
 };
